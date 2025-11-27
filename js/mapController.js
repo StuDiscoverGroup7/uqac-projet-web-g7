bca009b1bd4f46ad6f6a429e17ff8d5053638507
Adrien Capdeville <adrien.capdeville@etu.univ-tlse2.fr>
2025-11-27 14:19:45 -0500

    Added dynamic offers by JSON (test)

--- js/mapController.js
+++ js/mapController.js
@@ -6,12 +6,40 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Example marker with popup
-const marker1 = L.marker([46.8139, -71.2080]).addTo(map)
-    .bindPopup('Au bon gougnier').openPopup();
+jsonTest = [
+    { "name": "Au bon gougnier",
+      "description": "Le restaurant Au Bon Gougnier, niché au cœur du Vieux‑Québec, est une adresse aussi chaleureuse qu'intrigante. Spécialisé dans la viande de dière (une coupe fictive tendre et persillée), il propose une cuisson unique « à la gougnière »",
+      "address": "331 boul. Tablot",
+      "offerLabel": "Offre d'emploi",
+      "offerType": "jobOffer",
+      "lat": 46.8150, 
+      "lng": -71.2100 
+    },
+    { "name": "Chez Tremblay",
+      "description": "Delicious sandwiches and salads.",
+      "address": "330 boul. Tablot",
+      "offerLabel": "Promotion",
+      "offerType": "promo",
+      "lat": 46.8120,
+      "lng": -71.2050
+    }
+]
 
-const marker2 = L.marker([46.8130, -71.2040]).addTo(map)
-    .bindPopup('Chez Tremblay').openPopup();
+jsonTest.forEach(offer => {
+    L.marker([offer.lat, offer.lng]).addTo(map).bindPopup('<b>' + offer.name + '</b><br>' + offer.description + '<br><i>' + offer.address + '</i><br><b>' + offer.offerLabel + '</b>');
+    $("#offersList ul").append(
+        '<li>' +
+        '<div>' +
+        '<div class="offerType">' + '<h3>' + offer.name + '</h3>' + '<h4 class="' + offer.offerType + '">' + offer.offerLabel + '</h4>' +  '</div>' +
+        '<hr>' +
+        '<h4>' + offer.address + '</h4>' +
+        '<p>' + offer.description + '</p>' +
+        '</div>' +
+        '</li>'
+    );
+})
+
+
 
 // Add scale control
 L.control.scale().addTo(map);
