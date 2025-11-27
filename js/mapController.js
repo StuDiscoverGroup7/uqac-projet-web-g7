1819ccbb72ee909e3a8e12e816baa41cc5f2923b
Adrien Capdeville <adrien.capdeville@etu.univ-tlse2.fr>
2025-11-27 15:24:17 -0500

    Enabled opening offers (test)

--- js/mapController.js
+++ js/mapController.js
@@ -1,3 +1,8 @@
+// popup function
+function closePopup() {
+    $("#popupInspection").remove();
+}
+
 // map controller
 const map = L.map('mapController').setView([46.8139, -71.2080], 13);
 
@@ -9,14 +14,30 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 jsonTest = [
     { "name": "Au bon gougnier",
       "description": "Le restaurant Au Bon Gougnier, niché au cœur du Vieux‑Québec, est une adresse aussi chaleureuse qu'intrigante. Spécialisé dans la viande de dière (une coupe fictive tendre et persillée), il propose une cuisson unique « à la gougnière »",
+      "promotionDescription": "Cherche stagiaire !",
+      "placeType": "Restaurant",
       "address": "331 boul. Tablot",
       "offerLabel": "Offre d'emploi",
       "offerType": "jobOffer",
       "lat": 46.8150, 
       "lng": -71.2100 
     },
-    { "name": "Chez Tremblay",
-      "description": "Delicious sandwiches and salads.",
+    { 
+      "name": "Chez Tremblay",
+      "description": "Librairie indépendante offrant une vaste sélection de livres neufs et d'occasion, ainsi que des événements littéraires réguliers.",
+      "promotionDescription": "19% de rabais sur votre première commande!",
+      "placeType": "Librairie",
+      "address": "330 boul. Tablot",
+      "offerLabel": "Promotion",
+      "offerType": "promo",
+      "lat": 46.8120,
+      "lng": -71.2050
+    },
+    { 
+      "name": "Chez Tremblay",
+      "description": "Librairie indépendante offrant une vaste sélection de livres neufs et d'occasion, ainsi que des événements littéraires réguliers.",
+      "promotionDescription": "19% de rabais sur votre première commande!",
+      "placeType": "Librairie",
       "address": "330 boul. Tablot",
       "offerLabel": "Promotion",
       "offerType": "promo",
@@ -27,27 +48,34 @@ jsonTest = [
 
 jsonTest.forEach(offer => {
     L.marker([offer.lat, offer.lng]).addTo(map).bindPopup('<b>' + offer.name + '</b><br>' + offer.description + '<br><i>' + offer.address + '</i><br><b>' + offer.offerLabel + '</b>');
-    $("#offersList ul").append(
-        '<li>' +
+    offerDom = $('<li>' +
         '<div>' +
         '<div class="offerType">' + '<h3>' + offer.name + '</h3>' + '<h4 class="' + offer.offerType + '">' + offer.offerLabel + '</h4>' +  '</div>' +
         '<hr>' +
         '<h4>' + offer.address + '</h4>' +
         '<p>' + offer.description + '</p>' +
         '</div>' +
-        '</li>'
-    );
+        '</li>');
+
+    offerDom.click(function(event) {
+        console.log(event.target)
+        $("body").append(`
+            <div id="popupInspection">
+                <div>
+                    <h1>${offer.name}</h1>
+                    <h3>${offer.placeType}</h3>
+                    <hr>
+                    <h4>${offer.description}</h4>
+                    <hr>
+                    <h4>${offer.promotionDescription}</h4>
+                    <button onClick="closePopup()">Fermer</button>
+                </div>
+            </div>
+        `);
+    })
+
+    $("#offersList ul").append(offerDom);
 })
-
-
 
 // Add scale control
 L.control.scale().addTo(map);
-
-// Click on map to show coordinates in a popup
-map.on('click', function(e) {
-    L.popup()
-        .setLatLng(e.latlng)
-        .setContent('Lat: ' + e.latlng.lat.toFixed(5) + '<br>Lng: ' + e.latlng.lng.toFixed(5))
-        .openOn(map);
-});
