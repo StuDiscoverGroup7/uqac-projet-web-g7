84e63e6fdbf81b5b411902ff323cf4e931cb8a48
Oscar Neveux <oscar.neveux@gmail.com>
2025-12-11 12:36:39 -0500

    Add colored type badges for offers in sidebar

--- js/mapController.js
+++ js/mapController.js
@@ -35,6 +35,18 @@ function createPopupHtml(offer) {
   `;
 }
 
+// Fonction pour obtenir le label et la classe CSS selon le type
+function getTypeInfo(type) {
+  const typeMap = {
+    reduction: { label: "Réduction", class: "type-reduction" },
+    job: { label: "Offre d'emploi", class: "type-job" },
+    stage: { label: "Stage", class: "type-stage" },
+    promotion: { label: "Promotion", class: "type-promotion" },
+    evenement: { label: "Événement", class: "type-evenement" },
+  };
+  return typeMap[type] || { label: type, class: "type-default" };
+}
+
 // Fonction pour afficher les offres
 function updateOffersDisplay(offers) {
   markers.forEach((marker) => {
@@ -51,33 +63,36 @@ function updateOffersDisplay(offers) {
   offers.forEach((offer) => {
     const lat = parseFloat(offer.latitude);
     const lng = parseFloat(offer.longitude);
+    const typeInfo = getTypeInfo(offer.type);
 
     // Ajouter un marqueur sur la carte
     const marker = L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(
-      "<b>" +
+      .addTo(map)
+      .bindPopup(
+        "<b>" +
           offer.title +
-        "</b><br>" +
-        offer.description +
-        "<br><i>" +
-        offer.address +
-        "</i><br><b>" +
-          offer.type +
-        "</b>"
-    );
+          "</b><br>" +
+          offer.description +
+          "<br><i>" +
+          offer.address +
+          "</i><br><b>" +
+          typeInfo.label +
+          "</b>"
+      );
     markers.push(marker);
 
     // Créer l'élément HTML pour la liste
     const offerHtml =
-    "<li>" +
+      "<li>" +
       "<div>" +
       '<div class="offerType">' +
       "<h3>" +
       offer.title +
       "</h3>" +
-      "<h4>" +
-      offer.type +
+      '<h4 class="' +
+      typeInfo.class +
+      '">' +
+      typeInfo.label +
       "</h4>" +
       "</div>" +
       "<hr>" +
@@ -95,10 +110,10 @@ function updateOffersDisplay(offers) {
     // Quand on clique sur une offre dans la liste
     offerDom.click(function () {
       $("body").append(createPopupHtml(offer));
-  });
+    });
 
-  $("#offersList ul").append(offerDom);
-});
+    $("#offersList ul").append(offerDom);
+  });
 }
 
 // Rendre cette fonction accessible depuis les autres fichiers
